f3e717c4b4b56f12dbfa165f4abc743bc52797d4
Siddhant Mittal <sidmit2001@gmail.com>
2021-05-31 00:16:50 +0530

    Added Homepage Component and Styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Homepage from './pages/homepage/homepage.components';
 
 function App() {
-  return <div>HELLO WORLD</div>;
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
