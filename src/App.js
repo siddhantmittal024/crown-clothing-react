d0e0a908a3132c83c8943b22331b3e1918792d35
Siddhant Mittal <sidmit2001@gmail.com>
2021-05-31 22:30:25 +0530

    Added Shop Data and Item Collection Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import Homepage from './pages/homepage/homepage.components';
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,22 @@
+import CollectionItem from '../collection-item/collection-item.component';
+import './collection-preview.styles.scss';
+
+const PreviewCollection = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, index) => index < 4)
+        .map((item) => (
+          <CollectionItem
+            key={item.id}
+            imageUrl={item.imageUrl}
+            price={item.price}
+            name={item.name}
+          />
+        ))}
+    </div>
+  </div>
+);
+
+export default PreviewCollection;
