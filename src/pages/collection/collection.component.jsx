8b2298e21cce32fa0a962634289a93707d4bab1c
Siddhant Mittal <sidmit2001@gmail.com>
2021-06-03 23:26:35 +0530

    Fixing Routes!!

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,9 @@
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => (
+  <div className="collection-page">
+    <h2>CATEGORY PAGE</h2>
+  </div>
+);
+
+export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import CollectionPage from '../collection/collection.component'
 import CollectionsOverview from '../../components/collection-overview/collection-overview.component';
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
   </div>
 );
 
