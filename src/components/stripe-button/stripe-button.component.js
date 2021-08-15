f152eea565951dc61c17538a8e675df488b92c86
Siddhant Mittal <sidmit2001@gmail.com>
2021-08-15 19:50:33 +0530

    Deploying!

--- src/components/stripe-button/stripe-button.component.js
+++ src/components/stripe-button/stripe-button.component.js
@@ -12,7 +12,7 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51IkWd7SDFnEwkem8hfEq8HMAG62nlPQjNvN1PulIZ6rxI7uR5C7kJSfNYWiMJJBTnpVQbxNo2l9OVfkOqhgePhqA00teFJWLDX';
   return (
     <StripeCheckout
-      label="Pay Now"
+      label="Pay Now 💳"
       name="CROWN CLOTHING Ltd."
       billingAddress
       shippingAddress
