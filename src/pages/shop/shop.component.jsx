c713cf495bc9f7b572c1fa3fa0d4ca6e9e83df41
Siddhant Mittal <sidmit2001@gmail.com>
2021-08-23 21:33:07 +0530

    Asynchronous Redux(Using Promise Pattern)!

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -21,7 +21,8 @@ class ShopPage extends React.Component {
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
-    collectionRef.onSnapshot(async (snapShot) => {
+
+    collectionRef.get().then((snapShot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapShot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
