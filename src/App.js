5b0cf0ac531094276bbc1656f0055e6ba2af97a0
Siddhant Mittal <sidmit2001@gmail.com>
2021-10-16 23:28:14 +0530

    refactoring

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { selectCurrentUser } from './redux/user/userSelectors';
+import CheckoutPage from './pages/checkout/checkout.component';
+import { checkUserSession } from './redux/user/userAction';
+
+const App = () => {
+  const currentUser = useSelector(selectCurrentUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(checkUserSession());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route exact path="/checkout" component={CheckoutPage} />
+        <Route
+          exact
+          path="/signin"
+          render={() =>
+            currentUser ? <Redirect to="/" /> : <SignInAndSignUp />
+          }
+        />
+      </Switch>
+    </div>
+  );
+};
+
+export default App;
