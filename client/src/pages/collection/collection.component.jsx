9819ae4c7c067af29f25bc9601d6cdc34af54df9
Siddhant Mittal <sidmit2001@gmail.com>
2021-12-22 00:34:22 +0530

    making mobile responsive

--- client/src/pages/collection/collection.component.jsx
+++ client/src/pages/collection/collection.component.jsx
@@ -1,24 +1,29 @@
-import './collection.styles.scss';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import React from 'react';
+import { connect } from 'react-redux';
 import { selectCollection } from '../../redux/shop/shopSelector';
 import CollectionItem from '../../components/collection-item/collection-item.component';
+import {
+  CollectionPageContainer,
+  CollectionTitle,
+  CollectionItemsContainer
+} from './collection.styles';
 
-const CollectionPage = () => {
-  const { collectionId } = useParams();
-  const collection = useSelector(selectCollection(collectionId));
-  //console.log(collection);
+const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
+    <CollectionPageContainer>
+      <CollectionTitle>{title}</CollectionTitle>
+      <CollectionItemsContainer>
         {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </CollectionItemsContainer>
+    </CollectionPageContainer>
   );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
