c8f40e35eb37fa36c31d4b23737d8aa388413bcb
Siddhant Mittal <sidmit2001@gmail.com>
2021-12-21 23:41:25 +0530

    making mobile responsive

--- client/src/components/collection-item/collection-item.component.jsx
+++ client/src/components/collection-item/collection-item.component.jsx
@@ -1,23 +1,33 @@
-import CustomButton from '../custom-button/custom-button.component';
-import './collection-item.styles.scss';
+import React from 'react';
 import { connect } from 'react-redux';
+
 import { addItem } from '../../redux/cart/cartAction';
 
-const CollectionItem = ({ item, addItem }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{ backgroundImage: `url(${item.imageUrl})` }}
-    />
-    <div className="collection-footer">
-      <span className="name">{item.name}</span>
-      <span className="price">{`$${item.price}`}</span>
-    </div>
-    <CustomButton inverted onClick={() => addItem(item)}>
-      Add to cart
-    </CustomButton>
-  </div>
-);
+import {
+  CollectionItemContainer,
+  CollectionFooterContainer,
+  AddButton,
+  BackgroundImage,
+  NameContainer,
+  PriceContainer
+} from './collection-item.styles';
+
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+
+  return (
+    <CollectionItemContainer>
+      <BackgroundImage className="image" imageUrl={imageUrl} />
+      <CollectionFooterContainer>
+        <NameContainer>{name}</NameContainer>
+        <PriceContainer>{price}</PriceContainer>
+      </CollectionFooterContainer>
+      <AddButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </AddButton>
+    </CollectionItemContainer>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   addItem: (item) => dispatch(addItem(item))
