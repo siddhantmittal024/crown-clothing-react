8a9ab0677f8acb8b7ab98237389291a326829f8f
Siddhant Mittal <sidmit2001@gmail.com>
2021-12-22 00:37:08 +0530

    making mobile responsive

--- client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,15 +1,15 @@
-import './sign-in-and-sign-up.styles.scss';
 import React from 'react';
+
 import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
 
-const SignInAndSignUp = () => {
-  return (
-    <div className="sign-in-and-sign-up">
-      <SignIn />
-      <SignUp />
-    </div>
-  );
-};
+import { SignInAndSignUpContainer } from './sign-in-and-sign-up.styles';
 
-export default SignInAndSignUp;
+const SignInAndSignUpPage = () => (
+  <SignInAndSignUpContainer>
+    <SignIn />
+    <SignUp />
+  </SignInAndSignUpContainer>
+);
+
+export default SignInAndSignUpPage;
