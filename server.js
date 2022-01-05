5052af3c4746fe9d4eb712396be085434cf9ebcc
Siddhant Mittal <sidmit2001@gmail.com>
2022-01-05 21:13:11 +0530

    fixes

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
-
   app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
